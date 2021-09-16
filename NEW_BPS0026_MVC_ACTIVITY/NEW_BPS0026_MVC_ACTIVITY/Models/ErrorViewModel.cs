87bce88f35bda2f9deeab12d129170e31e0b969c
fbensibpsolutions <f.bensi@bpsolutions.biz>
2021-09-17 07:45:14 +0800

    Add source code

---
+++ NEW_BPS0026_MVC_ACTIVITY/NEW_BPS0026_MVC_ACTIVITY/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace NEW_BPS0026_MVC_ACTIVITY.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
